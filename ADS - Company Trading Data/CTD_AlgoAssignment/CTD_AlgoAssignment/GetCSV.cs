cc3c8c07af5854d77bd6451cec15f6a472a94968
Matrix374 <rumahirfan@gmail.com>
2019-04-12 14:49:42 +0100

    Company Class Implemented, Basic CSV Read Functionality Added

---
+++ ADS - Company Trading Data/CTD_AlgoAssignment/CTD_AlgoAssignment/GetCSV.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTD_AlgoAssignment
+{
+    class GetCSV
+    {
+        public GetCSV(string directory)
+        {
+            try
+            {
+                string data = File.ReadAllText(directory);
+                Debug.WriteLine(data);
+            }
+            catch(Exception e)
+            {
+                Debug.WriteLine("File could not be read");
+            }
+        }
+    }
+}
